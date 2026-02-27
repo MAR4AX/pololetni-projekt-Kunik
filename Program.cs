64c92768c63d2f0c52e188069037a2709f547cf5
MarekK <marek.kunik08@gmail.com>
2026-02-27 19:20:52 +0100

    Vylepšení základních tříd, začátky loginu

--- Program.cs
+++ Program.cs
@@ -1,11 +1,61 @@
-﻿using System.Reflection.Metadata;
-using System.Xml.Linq;
-User admin = new User("Admin", "Admin", true, 0, "none", "admin");
-admin.NewEmployee("Marek", "Kunik", true, 0, "kunik.ma.2024@skola.ssps.cz", "heslo123");
+﻿Library library = new Library("knihovna");
+library.NewEmployee("Marek", "Kuník", true, 0, "kunik.ma.2024@skola.ssps.cz", "heslo123");
 
+string LogInContact;
+string LogInPassword;
 while (true)
 {
+    Console.WriteLine("Zadejte e-mail nebo telefonní číslo");
+    LogInContact = Console.ReadLine();
+    Console.WriteLine("Zadejte heslo");
+    LogInPassword = Console.ReadLine();
 
+    foreach (User user in library.users)
+    {
+       if(LogInContact == user.contact &&  LogInPassword == user.password)
+        {
+            Console.WriteLine();
+        }
+    }
+}
+
+class Library
+{
+    public string name;
+
+    public List<Book> books;
+    public List<User> readers;
+    public List<User> employees;
+    public List<User> users;
+
+    public Library(string name)
+    {
+        this.name = name;
+
+        books = new List<Book>();
+        readers = new List<User>();
+        employees = new List<User>();
+        users = new List<User>();
+    }
+    public void NewBook(string name, string author, int ISBN, int year, string genre, int exemplars)
+    {
+        Book book = new Book(name, author, ISBN, year, genre, exemplars);
+        books.Add(book);
+    }
+
+    public void NewReader(string name, string surname, bool isEmployee, int readerNumber, string contact, string password)
+    {
+        User user = new User(name, surname, isEmployee=false, readerNumber, contact, password);  
+        readers.Add(user);
+        users.Add(user);
+    }
+
+    public void NewEmployee(string name, string surname, bool isEmployee, int readerNumber, string contact, string password)
+    {
+        User user = new User(name, surname, isEmployee=true, readerNumber=0, contact, password);
+        employees.Add(user);
+        users.Add(user);
+    }
 }
 class User
 {
@@ -25,32 +75,9 @@ class User
         this.contact = contact;
         this.password = password;
 
-        books = new List<Book>();
-        readers = new List<User>();
-        employees = new List<User>();
     }
 
-    public List<Book> books;
-    public List<User> readers;
-    public List<User> employees;
 
-    public void NewBook(string name, string author, int ISBN, int year, string genre, int exemplars)
-    {
-        Book book = new Book(name, author, ISBN, year, genre, exemplars);
-        books.Add(book);
-    }
-
-    public void NewReader(string name, string surname, bool isEmployee, int readerNumber, string contact, string password)
-    {
-        User reader = new User(name, surname, isEmployee=false, readerNumber, contact, password);  
-        readers.Add(reader);
-    }
-
-    public void NewEmployee(string name, string surname, bool isEmployee, int readerNumber, string contact, string password)
-    {
-        User employee = new User(name, surname, isEmployee=true, readerNumber, contact, password);
-        employees.Add(employee);
-    }
 }
 
 class Book
